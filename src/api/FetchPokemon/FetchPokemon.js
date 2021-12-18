4256fd30ae73729d9d4ecedaa5ed74c9b9d55d12
Dylan Dabrowski <57879165+DylanDabrowski@users.noreply.github.com>
2021-12-18 03:31:00 -0500

    added 2 fonts

--- src/api/FetchPokemon/FetchPokemon.js
+++ src/api/FetchPokemon/FetchPokemon.js
@@ -28,10 +28,41 @@ function addItem(item) {
   if (item.type === "poison") {
     type.style.backgroundColor = "#9b69da";
   } else if (item.type === "fire") {
-    type.style.backgroundColor = "#9b69da";
+    type.style.backgroundColor = "#fd4b5a";
+  } else if (item.type === "water") {
+    type.style.backgroundColor = "#5280e6";
+  } else if (item.type === "bug") {
+    type.style.backgroundColor = "#3c9950";
+  } else if (item.type === "flying") {
+    type.style.backgroundColor = "#94b2c7";
+  } else if (item.type === "normal") {
+    type.style.backgroundColor = "#ca98a6";
+  } else if (item.type === "electric") {
+    type.style.backgroundColor = "#e2e32b";
+    type.style.color = "#0f0f0f";
+  } else if (item.type === "ground") {
+    type.style.backgroundColor = "#6e491f";
+  } else if (item.type === "fairy") {
+    type.style.backgroundColor = "#eda5f0";
+  } else if (item.type === "grass") {
+    type.style.backgroundColor = "#27cb50";
+  } else if (item.type === "fighting") {
+    type.style.backgroundColor = "#ef6239";
+  } else if (item.type === "psychic") {
+    type.style.backgroundColor = "#f71d92";
+  } else if (item.type === "steel") {
+    type.style.backgroundColor = "#43bd94";
+  } else if (item.type === "dark") {
+    type.style.backgroundColor = "#343448";
+  } else if (item.type === "dragon") {
+    type.style.backgroundColor = "#448a95";
+  } else if (item.type === "ghost") {
+    type.style.backgroundColor = "#906791";
+  } else if (item.type === "ice") {
+    type.style.backgroundColor = "#86d2f5";
   } else {
     type.style.backgroundColor = "#f2f2f2";
-    type.style.color = "#808080";
+    type.style.color = "#050505";
   }
 
   const desc = document.createElement("p");
@@ -49,11 +80,6 @@ function addItem(item) {
 }
 
 export default class FetchPokemon extends React.Component {
-  state = {
-    loading: true,
-    pokemons: null,
-  };
-
   async componentDidMount() {
     fetch("http://localhost:5000/api/pokemon", {
       method: "GET",
@@ -69,9 +95,11 @@ export default class FetchPokemon extends React.Component {
     return (
       <div className="poke">
         <div className="pokeWrapper">
-          <h2 className="title">Pokemon</h2>
+          <h2 className="title">Pokedex</h2>
           <div id="pokemonCards" className="pokemonCards"></div>
         </div>
+
+        <div className="popup"></div>
       </div>
     );
   }
